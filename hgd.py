4dfc65c45d59b62f47a979e1cd8d1e38fd833507
vibhormishra123 <111233049+vibhormishra123@users.noreply.github.com>
2022-12-24 01:06:15 -0800

    Add files via upload

---
+++ hgd.py
@@ -0,0 +1,7 @@
+
+def findSum(n) :
+	return n * (n + 1) / 2
+	
+n = 5
+print findSum(n)
+
---
+++ iuy.py
@@ -0,0 +1,10 @@
+
+def getSum(n):
+	
+	sum = 0
+	for digit in str(n):
+	sum += int(digit)	
+	return sum
+
+n = 12345
+print(getSum(n))
