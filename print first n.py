55051905e290a326a0cb663034f2a66920430d9f
vibhormishra123 <111233049+vibhormishra123@users.noreply.github.com>
2022-12-24 01:07:22 -0800

    Add files via upload

---
+++ print first n.py
@@ -0,0 +1,19 @@
+# Python program to print first n
+# Fibonacci numbers
+
+# Function to print first n
+# Fibonacci Numbers
+
+
+def printFibonacciNumbers(n):
+
+	f1 = 0
+	f2 = 1
+	if (n < 1):
+		return
+	print(f1, end=" ")
+	for x in range(1, n):
+		print(f2, end=" ")
+		next = f1 + f2
+		f1 = f2
+		f2 = ne
