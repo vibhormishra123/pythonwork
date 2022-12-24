060d142452fbe07253bac339d061d6b304e40e13
vibhormishra123 <111233049+vibhormishra123@users.noreply.github.com>
2022-12-24 00:27:17 -0800

    Add files via upload

---
+++ h6.py
@@ -0,0 +1,12 @@
+
+def multiple(m, n):
+
+	
+	a = range(n, (m * n)+1, n)
+	
+	print(*a)
+
+
+m = 4
+n = 3
+multiple(m, n)
---
+++ oddd.py
@@ -0,0 +1,6 @@
+print("====The First 10 Odd Natural Numbers====")
+i = 1
+
+while(i <= 10):
+    print(2 * i - 1)
+    i = i + 1
